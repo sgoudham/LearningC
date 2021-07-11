16201c0725ba2a0284c97e255fdd0a1a7d4d5e16
sgoudham <sgoudham@gmail.com>
2021-07-11 19:31:24 +0100

    Reformat file

--- hello.c
+++ hello.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
+
 void println(char *printString);
 
-int main() 
-{
-	println("Hello World!");
-	return 0;
+int main() {
+    println("Hello World!");
+    return 0;
 }
 
-void println(char *printString) 
-{
-	printf("%s\n", printString);
+void println(char *printString) {
+    printf("%s\n", printString);
 }
